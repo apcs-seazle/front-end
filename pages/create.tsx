25a4258b6067087dfd70a19355540255adc6e5c9
vbph <vbphung20@apcs.fitus.edu.vn>
2022-12-16 22:43:35 +0700

    init UI

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,34 +1,71 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
-export default function create(this:any) {
-  const [buffer,setBuffer] = React.useState<any>()
+export default function Create(this: any) {
+  const [content, setContent] = useState({} as any);
+  const [name, setName] = useState("");
+  const [desc, setDesc] = useState("");
 
-  function onSubmit (){
-    console.log("Submit ...");
-  }
-  
-  const captureFile =(event :any)=> {
-    const file = event.target.files[0]
-    const reader = new window.FileReader()
-    reader.readAsArrayBuffer(file)
-    reader.onloadend = ()=>{
-      setBuffer(reader.result)
-      console.log(reader.result)
-    }
-  }
+  const onSubmit = () => {
+    const rd = new window.FileReader()
+    rd.readAsArrayBuffer(content)
+    rd.onloadend = () => {
+      axios.put('', {
+        name, description: desc,
+      })
+    };
 
-  return (
-    <>
-      <div className="main">
-          <h1 >BO HINH VO DI MAY EM</h1>
-          <form >
-            <input type = "file" onChange={ event => captureFile(event)}/>
-          </form>
-          <button onClick ={onSubmit}>Submit</button>  
-        <div >
-          <h1> Image will appear here</h1>
+    const onPickContent = (ev: any) => {
+      setContent(ev["target"]["files"][0]);
+    };
+
+    return (
+      <section className="bg-gray-50 dark:bg-gray-900">
+        <div className="flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div className="px-96 py-24 space-y-12">
+            <h1 className="text-5xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white">
+              Create NFT
+            </h1>
+
+            <form className="space-y-4 md:space-y-6">
+              <div>
+                <label className="block mb-2 text-xl font-normal text-gray-900 dark:text-white">
+                  Name
+                </label>
+
+                <input
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Item Name"
+                  onVolumeChange={(ev) => {
+                    setName((ev.target as HTMLInputElement).value);
+                  }}
+                />
+              </div>
+
+              <div>
+                <label className="block mb-2 text-xl font-normal text-gray-900 dark:text-white">
+                  Description
+                </label>
+
+                <textarea
+                  rows={5}
+                  placeholder="Datailed description of your NFT"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  onVolumeChange={(ev) => {
+                    setDesc((ev.target as HTMLInputElement).value);
+                  }}
+                />
+              </div>
+
+              <button
+                className="bg-blue-700 text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                onClick={onSubmit}
+              >
+                Create NFT
+              </button>
+            </form>
+          </div>
         </div>
-        </div>
-    </>
-  );
-};
+      </section>
+    );
+  }
