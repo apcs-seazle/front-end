8a6436ce68afb7f4dd96c7ac7534530373cc0552
leviosarz <dungdq@pop-os.localdomain>
2022-12-23 11:57:01 +0700

    [add] page nft

--- pages/nft/index.tsx
+++ pages/nft/index.tsx
@@ -9,7 +9,7 @@ export default function NFTPage() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [item, setItem] = useState();
 
-  console.log(`before http://localhost:3030/nft/get/${query.id}`)
+
   useEffect(() => {
     fetch(`http://localhost:3030/nft/get/${query.id}`)
     .then((res) => res.json())
@@ -23,45 +23,16 @@ export default function NFTPage() {
         setError(error);
       }
       );
-    }, []);
-
-    // const useEffect = async () => {
-    //   await fetch(`http://localhost:3030/nft/get/${query.id}`)
-    //   .then((res) => res.json())
-    //   .then(
-    //     (result) => {
-    //       setIsLoaded(true);
-    //       setItem(result);
-    //     },
-    //     (error) => {
-    //       setIsLoaded(true);
-    //       setError(error);
-    //     }
-    //     );
-    //   }
-    console.log(`after http://localhost:3030/nft/get/${query.id}`)
-  
-    if (item != null) {
-      console.log('item not null')
-      console.log(`${item.contentUrl}`)
-    }
-    else
-      console.log('item null')
+    }, [query.id]);
 
   if (error) {
-    console.log("error")
     return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    console.log("loading")
+  } else if (!isLoaded || item.contentUrl == undefined) {
     return <div>Loading...</div>;
   } else {
-    console.log("image")
-    console.log(`wtf ?? http://localhost:3030/nft/get/${query.id}`)
     return <div>
-      <div>{`http://localhost:3030/nft/get/${query.id}`}</div>
-      <div>{item.contentUrl}</div>
-    <img src={item.contentUrl} alt="image" />
-      </div>
+      <img src={item.contentUrl} alt="image" />
+    </div>
   }
   
 }
