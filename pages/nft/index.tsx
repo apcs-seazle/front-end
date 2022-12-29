081d2c70850b581e10d84d57f95e3431b50c5c62
Le Nguyen Quang Minh <101281380+lnqminh3003@users.noreply.github.com>
2022-12-29 12:44:40 +0700

    done Buy nft and modal popup

--- pages/nft/index.tsx
+++ pages/nft/index.tsx
@@ -48,11 +48,14 @@ export default function NFTPage() {
                   value: valueInWei
                 });
                 setModalSuccess(true)
+                setShowModal(false)
+                setModalFail(false)
               }
               catch(err)
               {
-                console.log(err);
-                setModalFail(true)
+                setModalSuccess(false)
+                setModalFail(true);
+                setShowModal(false);
               }
           }
       }
@@ -77,8 +80,8 @@ export default function NFTPage() {
         </button>
         {showModal == true && 
         <div>
-            <div  className="bg-neutral-700 bg-opacity-40 fixed top-0 left-0 right-0 z-50 w-full p-4 overflw-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
-                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700 mt-20 mx-40">
+            <div  className="grid place-items-center bg-neutral-700 bg-opacity-40 fixed top-0 left-0 right-0 z-50 w-full p-4 overflw-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
+                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
                   <div className="flex items-start p-4 border-b rounded-t dark:border-gray-600">
                   <svg className="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
                     <h3 className="text-xl font-semibold pt-2 pl-4 text-gray-900 dark:text-white">
@@ -113,6 +116,64 @@ export default function NFTPage() {
             </div>
         </div>
        }
+       { modalSuccess == true &&
+       <div>
+          <div  className="grid place-items-center bg-neutral-700 bg-opacity-40 fixed top-0 left-0 right-0 z-50 w-full p-4 overflw-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
+                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700 w-96 grid place-items-center">
+                  <div className="flex items-start p-4 border-b rounded-t dark:border-gray-600">
+                  <img
+                  src="https://cdn-icons-png.flaticon.com/512/148/148767.png"
+                  className="p-1 rounded h-11 w-11"
+                  alt="..."
+                />
+                    <h3 className="text-xl font-semibold pt-2 pl-4 text-gray-900 dark:text-white">
+                      Payment successful
+                    </h3>
+                  </div>
+                  <div className="p-6 space-y-6">
+                    <p className="font-semibold text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                      THANK YOU
+                    </p>
+                  </div>
+                  <div className="flex items-center p-6 space-x-2  rounded-b dark:border-gray-600">
+                    <button onClick={()=>{setModalSuccess(false)}} data-modal-toggle="defaultModal" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Let's go
+                    </button>
+                  </div>
+                </div>
+              
+            </div>
+        </div>   
+       }
+       {modalFail == true &&
+        <div>
+          <div  className="grid place-items-center bg-neutral-700 bg-opacity-40 fixed top-0 left-0 right-0 z-50 w-full p-4 overflw-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
+                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700 w-96 grid place-items-center">
+                  <div className="flex items-start p-4 border-b rounded-t dark:border-gray-600">
+                  <img
+                  src="https://cdn-icons-png.flaticon.com/512/6659/6659895.png"
+                  className="p-1 rounded h-11 w-11"
+                  alt="..."
+                />
+                    <h3 className="text-xl font-semibold pt-2 pl-4 text-gray-900 dark:text-white">
+                      Fail Payment
+                    </h3>
+                  </div>
+                  <div className="p-6 space-y-6">
+                    <p className="font-semibold text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                      TRY AGAIN
+                    </p>
+                  </div>
+                  <div className="flex items-center p-6 space-x-2  rounded-b dark:border-gray-600">
+                    <button onClick={()=>{setModalFail(false)}} data-modal-toggle="defaultModal" type="button" className="text-white bg-red-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Let's go
+                    </button>
+                  </div>
+                </div>
+            </div>
+        </div>
+       }
+       
       </div>
     )
   }
