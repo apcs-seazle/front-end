727eecb1955254303f29fe5654222afc464493d4
Luong <hoangluongkhtn1552@gmail.com>
2023-01-06 10:52:56 +0700

    Update create.tsx
    fix detail NFTs

--- pages/create.tsx
+++ pages/create.tsx
@@ -139,7 +139,7 @@ export default function Create(this: any) {
 
               <textarea
                 rows={5}
-                placeholder="Datailed description of your NFT"
+                placeholder="Detailed description of your NFT"
                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 onChange={(ev) => {
                   setDesc((ev.target as any).value);
