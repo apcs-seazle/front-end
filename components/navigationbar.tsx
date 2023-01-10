c27bb7042a443e5bb2fb3b54397006d790f24ccc
leviosarz <dungdq2002@gmail.com>
2023-01-10 20:54:22 +0700

    [update] icon navigation bar

--- components/navigationbar.tsx
+++ components/navigationbar.tsx
@@ -91,17 +91,25 @@ const NavigationBar = (searchQuery: any) => {
           {/* Menu Items */}
           <div className="flex space-x-6">
             {/* By text */}
-            <div
-              onMouseEnter={handleClickDropdown.bind(null, "explore")}
-              onMouseLeave={handleClickDropdown.bind(null, "explore")}
+            <Link
+              href="/create"
+              className="block text-gray-700 rounded font-semibold hover:text-sky-600 hover:font-bold"
             >
-              <Link
-                href="/create"
-                className="block text-gray-700 rounded font-semibold hover:text-sky-600 hover:font-bold"
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2.3}
+                stroke="currentColor"
+                className="w-6 h-6"
               >
-                Create
-              </Link>
-            </div>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </Link>
 
             <GetMoney address={"GetMoney"} />
             {/* By icon */}
@@ -110,7 +118,7 @@ const NavigationBar = (searchQuery: any) => {
             {/* Wallet */}
             <WalletConnect />
             {/* Cart */}
-            <a href="#" className="block text-gray-700 hover:text-sky-600">
+            {/* <a href="#" className="block text-gray-700 hover:text-sky-600">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -125,7 +133,7 @@ const NavigationBar = (searchQuery: any) => {
                   d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
                 />
               </svg>
-            </a>
+            </a> */}
           </div>
         </div>
       </nav>
