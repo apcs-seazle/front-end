b97e8f96e6e160f15f0550bf43d0202945d52744
leviosarz <dungdq2002@gmail.com>
2023-01-11 00:19:45 +0700

    [add] update nft ->> note: DO NOT USE [DELETE ]

--- pages/nft/index.tsx
+++ pages/nft/index.tsx
@@ -8,6 +8,7 @@ import web3 from "../../helpers/connectMetamask/web3";
 import { HOST } from "../../utils/constant";
 import { result } from "lodash";
 import Link from "next/link";
+import axios from "axios";
 
 declare let window: any;
 
@@ -23,7 +24,8 @@ export default function NFTPage() {
   const [modalSuccess, setModalSuccess] = useState(false);
   const [modalFail, setModalFail] = useState(false);
 
-  console.log(query.id);
+  const [isEditting, setIsEditting] = useState(false);
+
   useEffect(() => {
     if (query.id == undefined) return;
     fetch(`${HOST}/nft/get/${query.id}`)
@@ -65,6 +67,26 @@ export default function NFTPage() {
     }
   };
 
+  function updateNFT() {
+    console.log(document.getElementById("p_name")?.textContent);
+    console.log(document.getElementById("p_description")?.textContent);
+    let newName = document.getElementById("p_name")?.textContent as string;
+    let newDescription = document.getElementById("p_description")
+      ?.textContent as string;
+    console.log("updateNFT");
+    axios
+      .post(`${HOST}/nft/update/${item._id}`, {
+        // name: "try",
+        name: newName,
+        description: newDescription,
+      })
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
+  }
+
   if (error) {
     return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
@@ -73,109 +95,71 @@ export default function NFTPage() {
     const fileUrl = (item.contentUrl as string) ?? "";
 
     return (
-      <div className="  ">
+      <div className="bg-[#F0F9FF] h-max">
         <NavigationBar />
-        <div className="flex flex-row gap-5 px-5 py-5">
-          <div className="flex flex-col w-2/5 border border-gray-200 rounded-2xl">
-            <div className="font-bold text-lg text-right pr-3">
-              <p className="text-gray-400 dark:text-gray-400 inline-block">1</p>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={2}
-                stroke="#8A939B"
-                className="w-6 h-6 ml-2 inline-block my-3"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                />
-              </svg>
-            </div>
+        <div className="flex flex-row gap-5 px-5 py-5 mt-10">
+          <div className="flex flex-col w-2/5 aspect-square  rounded-3xl ml-10  ">
             <img
               src={fileUrl}
               alt="image"
-              className="h-full w-full border-t border-gray-200 rounded-b-2xl"
+              className="h-full w-full border-t object-cover  rounded-3xl border-gray-200"
             />
           </div>
-          <span className="block w-3/5">
+          <span className="block w-2/5 mx-auto">
             <div className="flex flex-col">
-              <p className="text-[1.5rem] leading-relaxed text-sky-600 mt-2">
-                Collection name
-              </p>
-
-              <p className="text-black font-bold text-[3rem] mt-3 inline-block">
-                {item.name} #{item.idNFT}
-              </p>
-              <div className="flex flex-row">
-                <p className="text-black text-[1.3rem] inline-block">
-                  Owned by
-                  {
-                    <Link
-                      href={{ pathname: "./create" }}
-                      className="text-sky-600"
-                    >
-                      {" "}
-                      Dung De Quy
-                    </Link>
-                  }
-                </p>
-                <div className="font-bold text-[1.3rem] pl-5">
+              {/* <div className="flex items-center"> */}
+              <p
+                className="relative text-black font-bold text-[2rem] mt-3"
+                contentEditable={isEditting}
+                suppressContentEditableWarning={true}
+                id="p_name"
+              >
+                {item.name}
+                {isEditting == false ? ` #${item.idNFT} ` : ""}
+                {isEditting && (
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
-                    strokeWidth={2}
-                    stroke="#8A939B"
-                    className="w-8 h-8 inline-block"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6 absolute right-5 top-2 text-blue-600"
                   >
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
-                      d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-                    />
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                      d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
                     />
                   </svg>
-                  <p className="text-gray-400 dark:text-gray-400 font-medium pl-2 inline-block text-center">
-                    3
-                  </p>
-                </div>
-                <div className="font-bold text-[1.3rem] pl-3">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="#8A939B"
-                    viewBox="0 0 24 24"
-                    strokeWidth={2}
-                    stroke="#8A939B"
-                    className="w-8 h-8 ml-2 inline-block"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                    />
-                  </svg>
-
-                  <p className="text-gray-400 dark:text-gray-400 font-medium pl-2 inline-block text-center">
-                    3
-                  </p>
+                )}
+              </p>
+              {/* </div> */}
+              <div className="flex flex-row">
+                <p className="text-black text-[1.1rem] inline-block">
+                  Owned by
+                  {
+                    <Link href="#" className="text-sky-600">
+                      {" "}
+                      {item.ownerAddress}
+                    </Link>
+                  }
+                </p>
+              </div>
+              <div className="bg-amber-200 w-fit px-5 py-2 rounded-md my-5">
+                <div className="text-slate-500">Current price</div>
+                <div className="text-[2.5rem] font-semibold">
+                  {item.price} BNB
                 </div>
               </div>
               <div className="mt-2 flex flex-col border-2 border-[#8A939B] rounded-xl">
-                <div className="font-bold text-[1.5rem] text-left pl-3 py-4 items-center">
+                <div className="font-bold flex text-[1.2rem] pl-3 py-2 items-center">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
                     strokeWidth={1.5}
                     stroke="black"
-                    className="w-9 h-9 inline-block"
+                    className="w-7 h-7 inline-block"
                   >
                     <path
                       strokeLinecap="round"
@@ -187,35 +171,61 @@ export default function NFTPage() {
                   <p className="text-black font-medium dark:text-white inline-block pl-5">
                     Description
                   </p>
+                  {isEditting && (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6 ml-auto mr-5 inline-block text-blue-600"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                      />
+                    </svg>
+                  )}
                 </div>
-                <div className="h-96 flex-2 overflow-y-auto border-t-2 border-[#8A939B] px-3 py-3 font-normal text-black text-[1.3rem] text-left">
-                  <p className="">
-                    By
-                    {
-                      <Link
-                        href={{ pathname: "./create" }}
-                        className="text-sky-600 font-medium"
-                      >
-                        {" "}
-                        Dung De Quy
-                      </Link>
-                    }
-                    ,
-                  </p>
-                  <p className="pt-3">{item.description}</p>
-                </div>
-
-                <div className="flex flex-row border-t-2 border-[#8A939B]">
-                  <button
-                    onClick={() => {
-                      setShowModal(true);
-                    }}
-                    type="button"
-                    className="w-full m-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xl px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                <div className="h-96 flex-2 overflow-y-auto border-t-2 border-[#8A939B] px-3 font-normal text-black text-[1.1rem] text-left">
+                  <p
+                    className="pt-2"
+                    contentEditable={isEditting}
+                    suppressContentEditableWarning={true}
+                    id="p_description"
                   >
-                    Buy Now
-                  </button>
+                    {item.description}
+                  </p>
                 </div>
+                {item.ownerAddress == global.defaultAccount ? (
+                  <div className="flex flex-row border-t-2 border-[#8A939B]">
+                    <button
+                      onClick={() => {
+                        setShowModal(true);
+                      }}
+                      type="button"
+                      className="w-full m-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xl px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                    >
+                      Buy Now
+                    </button>
+                  </div>
+                ) : (
+                  <div className="flex flex-row border-t-2 border-[#8A939B]">
+                    <button
+                      onClick={() => {
+                        if (isEditting == true) updateNFT();
+                        setIsEditting(!isEditting);
+                      }}
+                      type="button"
+                      className={`w-full m-3 text-white ${
+                        isEditting ? "bg-red-700" : "bg-blue-700"
+                      } focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xl px-5 py-2.5  focus:outline-none `}
+                    >
+                      {isEditting ? "Done" : "Edit"}
+                    </button>
+                  </div>
+                )}
               </div>
             </div>
           </span>
@@ -253,9 +263,9 @@ export default function NFTPage() {
                     <p className="font-bold inline-block">ID:</p>
                     {" " + item.idNFT}
                   </p>
-                  {/* <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
                     Description: {item.description}
-                  </p> */}
+                  </p>
                   <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
                     <p className="font-bold inline-block">Owner Address:</p>
                     {" " + item.ownerAddress}
